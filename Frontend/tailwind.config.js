d26bb49513fd1904d426ed1e41291129d482489c
cerphh <curvesgt@gmail.com>
2025-05-01 23:47:03 +0800

    add tainwind css

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{html,js,ts,jsx,tsx}", // Tailwind will scan these files for class names
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
